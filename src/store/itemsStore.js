4151403391cad1f519e59581094c6c3bdbc40c6b
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-04-14 15:21:52 -0700

    adds context functions to itemsStore

---
+++ src/store/itemsStore.js
@@ -0,0 +1,70 @@
+import { create } from "zustand";
+import { initItems } from "../lib/constants";
+
+create((set) => ({
+  items: initItems,
+  addItem: (newItemText) => {
+    const newItem = {
+      id: new Date().getTime(),
+      name: newItemText,
+      checked: false,
+    };
+
+    set((state) => ({ items: [...state.items, newItem] }));
+  },
+
+  removeItem: (itemId) => {
+    set((state) => {
+      const updatedItems = state.items.filter((item) => item.id !== itemId);
+      return { items: updatedItems };
+    });
+  },
+
+  toggleItem: (itemId) => {
+    set((state) => {
+      const updatedItems = state.items.map((item) => {
+        if (item.id === itemId) {
+          return { ...item, checked: !item.checked };
+        }
+
+        return item;
+      });
+
+      return { items: updatedItems };
+    });
+  },
+
+  markAllComplete: () => {
+    set((state) => {
+      const updatedItems = state.items.map((item) => {
+        return { ...item, checked: true };
+      });
+
+      return { items: updatedItems };
+    });
+  },
+
+  markAllIncomplete: () => {
+    set((state) => {
+      const updatedItems = state.items.map((item) => {
+        return { ...item, checked: false };
+      });
+
+      return { items: updatedItems };
+    });
+  },
+
+  resetInitial: () => {
+    set(() => ({ items: initItems }));
+  },
+
+  removeAllItems: () => {
+    set(() => ({ items: [] }));
+  },
+
+  numCheckedItems: () => {
+    set((state) => ({
+      items: state.items.filter((item) => item.checked).length,
+    }));
+  },
+}));
